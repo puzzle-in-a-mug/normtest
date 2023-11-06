62ef5ecf773b047d6a3d28020c6698597033dcf1
Anderson Marcos Dias Canteli <andersonmdcanteli@gmail.com>
2023-11-06 11:26:31 -0300

    Update documentation.py

--- normtest/utils/documentation.py
+++ normtest/utils/documentation.py
@@ -1,4 +1,6 @@
 ### FUNCTIONS ###
+
+
 def docstring_parameter(*args, **kwargs):
     def dec(obj):
         obj.__doc__ = obj.__doc__.format(*args, **kwargs)
@@ -7,31 +9,18 @@ def docstring_parameter(*args, **kwargs):
     return dec
 
 
+### CONSTANTS ###
+
+
 ALPHA = {
     "type": "alpha : float, optional",
     "description": "The level of significance (:math:`\\alpha`). Must be ``0.01``, ``0.05`` (default) or ``0.10``;",
 }
 
 
-WEIGHTED = {
-    "type": "weighted : bool, optional",
-    "description": "Whether to estimate the Normal order considering the repeats as its average (`True`) or not (`False`, default). Only has an effect if the dataset contains repeated values;",
-}
-
-
-SAFE = {
-    "type": "safe : bool, optional",
-    "description": "Whether to check the inputs before performing the calculations (`True`) or not (`False`, default). Useful for beginners to identify problems in data entry (may reduce algorithm execution time);",
-}
-
-SAMPLE_SIZE = {
-    "type": "sample_size : int",
-    "description": "The sample size. Must be equal or greater than ``4``;",
-}
-
-X_DATA = {
-    "type": "x_data : :doc:`numpy array <numpy:reference/generated/numpy.array>`",
-    "description": "One dimension :doc:`numpy array <numpy:reference/generated/numpy.array>` with at least ``4`` observations.",
+CRITICAL = {
+    "type": "critical : float",
+    "description": "The critical value of the test;",
 }
 
 
@@ -44,26 +33,45 @@ CTE_ALPHA = {
         * `"1/2"`;""",
 }
 
-
-ZI = {
-    "type": "zi : :doc:`numpy array <numpy:reference/generated/numpy.array>`",
-    "description": "The statistical order in the standard Normal distribution scale.",
-}
-
-STATISTIC = {
-    "type": "statistic : float (positive)",
-    "description": "The test statistic;",
-}
-
 P_VALUE = {
     "type": "p_value : float or str",
     "description": "The probability of the test;",
 }
 
 
-CRITICAL = {
-    "type": "critical : float",
-    "description": "The critical value of the test;",
+SAFE = {
+    "type": "safe : bool, optional",
+    "description": "Whether to check the inputs before performing the calculations (`True`) or not (`False`, default). Useful for beginners to identify problems in data entry (may reduce algorithm execution time);",
+}
+
+
+SAMPLE_SIZE = {
+    "type": "sample_size : int",
+    "description": "The sample size. Must be equal or greater than ``4``;",
+}
+
+
+STATISTIC = {
+    "type": "statistic : float (positive)",
+    "description": "The test statistic;",
+}
+
+
+X_DATA = {
+    "type": "x_data : :doc:`numpy array <numpy:reference/generated/numpy.array>`",
+    "description": "One dimension :doc:`numpy array <numpy:reference/generated/numpy.array>` with at least ``4`` observations.",
+}
+
+
+WEIGHTED = {
+    "type": "weighted : bool, optional",
+    "description": "Whether to estimate the Normal order considering the repeats as its average (`True`) or not (`False`, default). Only has an effect if the dataset contains repeated values;",
+}
+
+
+ZI = {
+    "type": "zi : :doc:`numpy array <numpy:reference/generated/numpy.array>`",
+    "description": "The statistical order in the standard Normal distribution scale.",
 }
 
 

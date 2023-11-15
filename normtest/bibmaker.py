bc495f3184b8de0a147980b13a96714039dd42a8
Anderson Marcos Dias Canteli <andersonmdcanteli@gmail.com>
2023-11-14 23:20:27 -0300

    Update bibmaker.py

--- normtest/bibmaker.py
+++ normtest/bibmaker.py
@@ -1,3 +1,54 @@
+### DOCUMENTATTION ###
+def docstring_parameter(*args, **kwargs):
+    def dec(obj):
+        obj.__doc__ = obj.__doc__.format(*args, **kwargs)
+        return obj
+
+    return dec
+
+
+BIB = {
+    "type": "bib : file",
+    "description": "The `citekey.bib` file (only if `export=True`)",
+}
+CITATION = {
+    "type": "citation : str",
+    "description": "The citation generated",
+}
+CITEKEY = {
+    "type": "citekey : str",
+    "description": "The name that is used to uniquely identify the entry;",
+}
+
+DATE = {
+    "type": "date : str, optional",
+    "description": "The publication date. If `date=None`, the `year` parameter must not be `None`;",
+}
+
+
+EXPORT = {
+    "type": "export : bool, optional",
+    "description": "Whether to export the `citekey.bib` file (`True`, default) or not (`False`);",
+}
+
+
+OPTIONALS = {
+    "type": "**optionals : str",
+    "description": "Optional fields for an techreport entry (`volume`, `number`, `pages`, etc).",
+}
+
+YEAR = {
+    "type": "year : int, optional",
+    "description": "The year of publication; If `year=None`, the `date` parameter must not be `None`;",
+}
+
+
+# PARAM = {
+#     "type":
+#     "description":
+# }
+
+
 ### GENERAL ###
 def empty_spaces(fields, field):
     return len(max(fields, key=len)) - len(str(field))
@@ -362,134 +413,9 @@ GENERIC_FIELDS_DESCRIPTION = {
 }
 
 
-### TECHREPORT ###
+# ENTRYS #
 
-OPTIONAL_FIELDS_TECHREPORT = {
-    "type": GENERIC_FIELDS_DESCRIPTION["type"][1],
-    "subtitle": GENERIC_FIELDS_DESCRIPTION["subtitle"][1],
-    "titleaddon": GENERIC_FIELDS_DESCRIPTION["titleaddon"][1],
-    "language": GENERIC_FIELDS_DESCRIPTION["language"][1],
-    "number": GENERIC_FIELDS_DESCRIPTION["number"][1],
-    "version": GENERIC_FIELDS_DESCRIPTION["version"][1],
-    "note": GENERIC_FIELDS_DESCRIPTION["note"][1],
-    "location": GENERIC_FIELDS_DESCRIPTION["location"][1],
-    "month": GENERIC_FIELDS_DESCRIPTION["month"][1],
-    "isrn": GENERIC_FIELDS_DESCRIPTION["isrn"][1],
-    "eid": GENERIC_FIELDS_DESCRIPTION["eid"][1],
-    "chapter": GENERIC_FIELDS_DESCRIPTION["chapter"][1],
-    "pages": GENERIC_FIELDS_DESCRIPTION["pages"][1],
-    "pagetotal": GENERIC_FIELDS_DESCRIPTION["pagetotal"][1],
-    "addendum": GENERIC_FIELDS_DESCRIPTION["addendum"][1],
-    "pubstate": GENERIC_FIELDS_DESCRIPTION["pubstate"][1],
-    "doi": GENERIC_FIELDS_DESCRIPTION["doi"][1],
-    "eprint": GENERIC_FIELDS_DESCRIPTION["eprint"][1],
-    "eprintclass": GENERIC_FIELDS_DESCRIPTION["eprintclass"][1],
-    "eprinttype": GENERIC_FIELDS_DESCRIPTION["eprinttype"][1],
-    "url": GENERIC_FIELDS_DESCRIPTION["url"][1],
-    "urldate": GENERIC_FIELDS_DESCRIPTION["urldate"][1],
-}
-
-
-def make_techreport(
-    author,
-    title,
-    institution,
-    citekey,
-    year=None,
-    date=None,
-    export=True,
-    **optionals,
-):
-    """This function generates a `.bib` file for an `@techreport` entry.
-
-    Parameters
-    ----------
-    author : str
-        The person or persons who wrote the techreport. The authors' names must be separated by `" and "`;
-    title : str
-        The name of the techreport;
-    institution : str
-        The name of a university or some other institution, depending on the entry type. Traditional BibTeX uses the field name school for theses, which is supported as analias.
-    year : int, optional
-        The year of publication; If `year=None`, the `date` parameter must not be `None`;
-    date : str, optional
-        The publication date. If `date=None`, the `year` parameter must not be `None`;
-    citekey : str
-        The name that is used to uniquely identify the entry. If None, the algorithm will generate an automatic `citekey` based on the name of the authors and the year. See Notes for details;
-    export : bool, optional
-        Whether to export the `citekey.bib` file (`True`, default) or not (`False`);
-    **optionals : str
-        Optional fields for an techreport entry (`volume`, `number`, `pages`, etc).
-
-
-    Returns
-    -------
-    citation : str
-        The citation generated
-    bib : file
-        The `citekey.bib` file (only if `export=True`)
-
-
-    Notes
-    -----
-    A list of all optional fields, as well as their description, can be obtained using:
-
-    >>> from normtest import bibmaker
-    >>> for key, value in bibmaker.OPTIONAL_FIELDS_TECHREPORT.items():
-    >>>     print(key, ": ", value)
-
-    Examples
-    --------
-
-
-
-    """
-
-    if year is None and date is None:
-        try:
-            raise ValueError("FieldNotFoundError")
-        except ValueError:
-            print("Parameters `year` and `date` cannot be `None` at the same time.\n")
-            raise
-
-    field_template = "  {field_tag}{spaces} = {{{tag_value}}},\n"
-
-    fields = {
-        "author": author,
-        "title": title,
-        "institution": institution,
-        "year": year,
-        "date": date,
-    }
-    # getting all fields types
-    optional_fields = dict.fromkeys(OPTIONAL_FIELDS_TECHREPORT.keys())
-    optional_fields.update(optionals)
-    fields.update(optional_fields)
-
-    filtered = {k: v for k, v in fields.items() if v is not None}
-    fields.clear()
-    fields.update(filtered)
-
-    # building the citation
-    # first line:
-    citation = ["@techreport{{{citekey},\n".format(citekey=citekey)]
-
-    # ading fields
-    for key, value in fields.items():
-        spaces = " " * empty_spaces(fields.keys(), key)
-        citation.append(
-            field_template.format(field_tag=key, spaces=spaces, tag_value=value)
-        )
-    citation[-1] = citation[-1][:-2] + citation[-1][-1:]
-
-    citation.append("}")
-
-    if export:
-        with open(f"{citekey}.bib", "w") as my_bib:
-            for i in range(len(citation)):
-                my_bib.write(f"{citation[i]}")
-
-    return """""".join(citation)
+## Regular Types ##
 
 
 ### ARTICLE ###
@@ -532,6 +458,22 @@ OPTIONAL_FIELDS_ARTICLE = {
 }
 
 
+@docstring_parameter(
+    year=YEAR["type"],
+    year_description=YEAR["description"],
+    date=DATE["type"],
+    date_description=DATE["description"],
+    citekey=CITEKEY["type"],
+    citekey_description=CITEKEY["description"],
+    export=EXPORT["type"],
+    export_description=EXPORT["description"],
+    optionals=OPTIONALS["type"],
+    optionals_description=OPTIONALS["description"],
+    citation=CITATION["type"],
+    citation_description=CITATION["description"],
+    bib=BIB["type"],
+    bib_description=BIB["description"],
+)
 def make_article(
     author,
     title,
@@ -552,24 +494,24 @@ def make_article(
         The name of the article;
     journaltitle : str
         The name of a journal, a newspaper, or some other periodical.
-    year : int, optional
-        The year of publication; If `year=None`, the `date` parameter must not be `None`;
-    date : str, optional
-        The publication date. If `date=None`, the `year` parameter must not be `None`;
-    citekey : str
-        The name that is used to uniquely identify the entry. If None, the algorithm will generate an automatic `citekey` based on the name of the authors and the year. See Notes for details;
-    export : bool, optional
-        Whether to export the `citekey.bib` file (`True`, default) or not (`False`);
-    **optionals : str
-        Optional fields for an article entry (`volume`, `number`, `pages`, etc).
+    {year}
+        {year_description}
+    {date}
+        {date_description}
+    {citekey}
+        {citekey_description}
+    {export}
+        {export_description}
+    {optionals}
+        {optionals_description}
 
 
     Returns
     -------
-    citation : str
-        The citation generated
-    bib : file
-        The `citekey.bib` file (only if `export=True`)
+    {citation}
+        {citation_description}
+    {bib}
+        {bib_description}
 
 
     Notes
@@ -579,10 +521,6 @@ def make_article(
     >>> from normtest import bibmaker
     >>> for key, value in bibmaker.OPTIONAL_FIELDS_ARTICLE.items():
     >>>     print(key, ": ", value)
-
-    Examples
-    --------
-
 
 
     """
@@ -679,6 +617,22 @@ OPTIONAL_FIELDS_BOOK = {
 }
 
 
+@docstring_parameter(
+    year=YEAR["type"],
+    year_description=YEAR["description"],
+    date=DATE["type"],
+    date_description=DATE["description"],
+    citekey=CITEKEY["type"],
+    citekey_description=CITEKEY["description"],
+    export=EXPORT["type"],
+    export_description=EXPORT["description"],
+    optionals=OPTIONALS["type"],
+    optionals_description=OPTIONALS["description"],
+    citation=CITATION["type"],
+    citation_description=CITATION["description"],
+    bib=BIB["type"],
+    bib_description=BIB["description"],
+)
 def make_book(
     author,
     title,
@@ -696,24 +650,24 @@ def make_book(
         The person or persons who wrote the book. The authors' names must be separated by `" and "`;
     title : str
         The name of the book;
-    year : int, optional
-        The year of publication; If `year=None`, the `date` parameter must not be `None`;
-    date : str, optional
-        The publication date. If `date=None`, the `year` parameter must not be `None`;
-    citekey : str
-        The name that is used to uniquely identify the entry. If None, the algorithm will generate an automatic `citekey` based on the name of the authors and the year. See Notes for details;
-    export : bool, optional
-        Whether to export the `citekey.bib` file (`True`, default) or not (`False`);
-    **optionals : str
-        Optional fields for an article entry (`volume`, `number`, `pages`, etc).
+    {year}
+        {year_description}
+    {date}
+        {date_description}
+    {citekey}
+        {citekey_description}
+    {export}
+        {export_description}
+    {optionals}
+        {optionals_description}
 
 
     Returns
     -------
-    citation : str
-        The citation generated
-    bib : file
-        The `citekey.bib` file (only if `export=True`)
+    {citation}
+        {citation_description}
+    {bib}
+        {bib_description}
 
 
     Notes
@@ -747,7 +701,7 @@ def make_book(
         "date": date,
     }
     # getting all fields types
-    optional_fields = dict.fromkeys(OPTIONAL_FIELDS_ARTICLE.keys())
+    optional_fields = dict.fromkeys(OPTIONAL_FIELDS_BOOK.keys())
     optional_fields.update(optionals)
     fields.update(optional_fields)
 
@@ -758,6 +712,280 @@ def make_book(
     # building the citation
     # first line:
     citation = ["@book{{{citekey},\n".format(citekey=citekey)]
+
+    # ading fields
+    for key, value in fields.items():
+        spaces = " " * empty_spaces(fields.keys(), key)
+        citation.append(
+            field_template.format(field_tag=key, spaces=spaces, tag_value=value)
+        )
+    citation[-1] = citation[-1][:-2] + citation[-1][-1:]
+
+    citation.append("}")
+
+    if export:
+        with open(f"{citekey}.bib", "w") as my_bib:
+            for i in range(len(citation)):
+                my_bib.write(f"{citation[i]}")
+
+    return """""".join(citation)
+
+
+### TECHREPORT ###
+
+OPTIONAL_FIELDS_TECHREPORT = {
+    "type": GENERIC_FIELDS_DESCRIPTION["type"][1],
+    "subtitle": GENERIC_FIELDS_DESCRIPTION["subtitle"][1],
+    "titleaddon": GENERIC_FIELDS_DESCRIPTION["titleaddon"][1],
+    "language": GENERIC_FIELDS_DESCRIPTION["language"][1],
+    "number": GENERIC_FIELDS_DESCRIPTION["number"][1],
+    "version": GENERIC_FIELDS_DESCRIPTION["version"][1],
+    "note": GENERIC_FIELDS_DESCRIPTION["note"][1],
+    "location": GENERIC_FIELDS_DESCRIPTION["location"][1],
+    "month": GENERIC_FIELDS_DESCRIPTION["month"][1],
+    "isrn": GENERIC_FIELDS_DESCRIPTION["isrn"][1],
+    "eid": GENERIC_FIELDS_DESCRIPTION["eid"][1],
+    "chapter": GENERIC_FIELDS_DESCRIPTION["chapter"][1],
+    "pages": GENERIC_FIELDS_DESCRIPTION["pages"][1],
+    "pagetotal": GENERIC_FIELDS_DESCRIPTION["pagetotal"][1],
+    "addendum": GENERIC_FIELDS_DESCRIPTION["addendum"][1],
+    "pubstate": GENERIC_FIELDS_DESCRIPTION["pubstate"][1],
+    "doi": GENERIC_FIELDS_DESCRIPTION["doi"][1],
+    "eprint": GENERIC_FIELDS_DESCRIPTION["eprint"][1],
+    "eprintclass": GENERIC_FIELDS_DESCRIPTION["eprintclass"][1],
+    "eprinttype": GENERIC_FIELDS_DESCRIPTION["eprinttype"][1],
+    "url": GENERIC_FIELDS_DESCRIPTION["url"][1],
+    "urldate": GENERIC_FIELDS_DESCRIPTION["urldate"][1],
+}
+
+
+@docstring_parameter(
+    year=YEAR["type"],
+    year_description=YEAR["description"],
+    date=DATE["type"],
+    date_description=DATE["description"],
+    citekey=CITEKEY["type"],
+    citekey_description=CITEKEY["description"],
+    export=EXPORT["type"],
+    export_description=EXPORT["description"],
+    optionals=OPTIONALS["type"],
+    optionals_description=OPTIONALS["description"],
+    citation=CITATION["type"],
+    citation_description=CITATION["description"],
+    bib=BIB["type"],
+    bib_description=BIB["description"],
+)
+def make_techreport(
+    author,
+    title,
+    institution,
+    citekey,
+    year=None,
+    date=None,
+    export=True,
+    **optionals,
+):
+    """This function generates a `.bib` file for an `@techreport` entry.
+
+    Parameters
+    ----------
+    author : str
+        The person or persons who wrote the techreport. The authors' names must be separated by `" and "`;
+    title : str
+        The name of the techreport;
+    institution : str
+        The name of a university or some other institution, depending on the entry type. Traditional BibTeX uses the field name school for theses, which is supported as analias.
+    {year}
+        {year_description}
+    {date}
+        {date_description}
+    {citekey}
+        {citekey_description}
+    {export}
+        {export_description}
+    {optionals}
+        {optionals_description}
+
+
+    Returns
+    -------
+    {citation}
+        {citation_description}
+    {bib}
+        {bib_description}
+
+
+    Notes
+    -----
+    A list of all optional fields, as well as their description, can be obtained using:
+
+    >>> from normtest import bibmaker
+    >>> for key, value in bibmaker.OPTIONAL_FIELDS_TECHREPORT.items():
+    >>>     print(key, ": ", value)
+
+
+
+    """
+
+    if year is None and date is None:
+        try:
+            raise ValueError("FieldNotFoundError")
+        except ValueError:
+            print("Parameters `year` and `date` cannot be `None` at the same time.\n")
+            raise
+
+    field_template = "  {field_tag}{spaces} = {{{tag_value}}},\n"
+
+    fields = {
+        "author": author,
+        "title": title,
+        "institution": institution,
+        "year": year,
+        "date": date,
+    }
+    # getting all fields types
+    optional_fields = dict.fromkeys(OPTIONAL_FIELDS_TECHREPORT.keys())
+    optional_fields.update(optionals)
+    fields.update(optional_fields)
+
+    filtered = {k: v for k, v in fields.items() if v is not None}
+    fields.clear()
+    fields.update(filtered)
+
+    # building the citation
+    # first line:
+    citation = ["@techreport{{{citekey},\n".format(citekey=citekey)]
+
+    # ading fields
+    for key, value in fields.items():
+        spaces = " " * empty_spaces(fields.keys(), key)
+        citation.append(
+            field_template.format(field_tag=key, spaces=spaces, tag_value=value)
+        )
+    citation[-1] = citation[-1][:-2] + citation[-1][-1:]
+
+    citation.append("}")
+
+    if export:
+        with open(f"{citekey}.bib", "w") as my_bib:
+            for i in range(len(citation)):
+                my_bib.write(f"{citation[i]}")
+
+    return """""".join(citation)
+
+
+### MVBOOK ###
+
+OPTIONAL_FIELDS_MVBOOK = {
+    "editor": GENERIC_FIELDS_DESCRIPTION["editor"][1],
+    "editora": GENERIC_FIELDS_DESCRIPTION["editora"][1],
+    "editorb": GENERIC_FIELDS_DESCRIPTION["editorb"][1],
+    "editorc": GENERIC_FIELDS_DESCRIPTION["editorc"][1],
+    "translator": GENERIC_FIELDS_DESCRIPTION["translator"][1],
+    "annotator": GENERIC_FIELDS_DESCRIPTION["annotator"][1],
+    "commentator": GENERIC_FIELDS_DESCRIPTION["commentator"][1],
+    "introduction": GENERIC_FIELDS_DESCRIPTION["introduction"][1],
+    "foreword": GENERIC_FIELDS_DESCRIPTION["foreword"][1],
+    "afterword": GENERIC_FIELDS_DESCRIPTION["afterword"][1],
+    "subtitle": GENERIC_FIELDS_DESCRIPTION["subtitle"][1],
+    "titleaddon": GENERIC_FIELDS_DESCRIPTION["titleaddon"][1],
+    "language": GENERIC_FIELDS_DESCRIPTION["language"][1],
+    "origlanguage": GENERIC_FIELDS_DESCRIPTION["origlanguage"][1],
+    "edition": GENERIC_FIELDS_DESCRIPTION["edition"][1],
+    "volumes": GENERIC_FIELDS_DESCRIPTION["volumes"][1],
+    "series": GENERIC_FIELDS_DESCRIPTION["series"][1],
+    "number": GENERIC_FIELDS_DESCRIPTION["number"][1],
+    "note": GENERIC_FIELDS_DESCRIPTION["note"][1],
+    "publisher": GENERIC_FIELDS_DESCRIPTION["publisher"][1],
+    "location": GENERIC_FIELDS_DESCRIPTION["location"][1],
+    "isbn": GENERIC_FIELDS_DESCRIPTION["isbn"][1],
+    "pagetotal": GENERIC_FIELDS_DESCRIPTION["pagetotal"][1],
+    "addendum": GENERIC_FIELDS_DESCRIPTION["addendum"][1],
+    "pubstate": GENERIC_FIELDS_DESCRIPTION["pubstate"][1],
+    "doi": GENERIC_FIELDS_DESCRIPTION["doi"][1],
+    "eprint": GENERIC_FIELDS_DESCRIPTION["eprint"][1],
+    "eprintclass": GENERIC_FIELDS_DESCRIPTION["eprintclass"][1],
+    "eprinttype": GENERIC_FIELDS_DESCRIPTION["eprinttype"][1],
+    "url": GENERIC_FIELDS_DESCRIPTION["url"][1],
+    "urldate": GENERIC_FIELDS_DESCRIPTION["urldate"][1],
+}
+
+
+def make_mvbook(
+    author,
+    title,
+    citekey,
+    year=None,
+    date=None,
+    export=True,
+    **optionals,
+):
+    """This function generates a `.bib` file for an `@mvbook` entry.
+
+    Parameters
+    ----------
+    author : str
+        The person or persons who wrote the  multi-volume book. The authors' names must be separated by `" and "`;
+    title : str
+        The name of the multi-volume book;
+    year : int, optional
+        The year of publication; If `year=None`, the `date` parameter must not be `None`;
+    date : str, optional
+        The publication date. If `date=None`, the `year` parameter must not be `None`;
+    citekey : str
+        The name that is used to uniquely identify the entry. If None, the algorithm will generate an automatic `citekey` based on the name of the authors and the year. See Notes for details;
+    export : bool, optional
+        Whether to export the `citekey.bib` file (`True`, default) or not (`False`);
+    **optionals : str
+        Optional fields for an article entry (`volume`, `number`, `pages`, etc).
+
+
+    Returns
+    -------
+    citation : str
+        The citation generated
+    bib : file
+        The `citekey.bib` file (only if `export=True`)
+
+
+    Notes
+    -----
+    A list of all optional fields, as well as their description, can be obtained using:
+
+    >>> from normtest import bibmaker
+    >>> for key, value in bibmaker.OPTIONAL_FIELDS_MVBOOK.items():
+    >>>     print(key, ": ", value)
+
+
+    """
+
+    if year is None and date is None:
+        try:
+            raise ValueError("FieldNotFoundError")
+        except ValueError:
+            print("Parameters `year` and `date` cannot be `None` at the same time.\n")
+            raise
+
+    field_template = "  {field_tag}{spaces} = {{{tag_value}}},\n"
+
+    fields = {
+        "author": author,
+        "title": title,
+        "year": year,
+        "date": date,
+    }
+    # getting all fields types
+    optional_fields = dict.fromkeys(OPTIONAL_FIELDS_MVBOOK.keys())
+    optional_fields.update(optionals)
+    fields.update(optional_fields)
+
+    filtered = {k: v for k, v in fields.items() if v is not None}
+    fields.clear()
+    fields.update(filtered)
+
+    # building the citation
+    # first line:
+    citation = ["@mvbook{{{citekey},\n".format(citekey=citekey)]
 
     # ading fields
     for key, value in fields.items():
